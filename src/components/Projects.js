25f1c5e8dd9cc0666d47c5efff9aa4c9b9093870
Aleksandar Krastinkov <akrastinkov@abv.bg>
2024-06-21 01:00:38 +0300

    Initial commit

---
+++ src/components/Projects.js
@@ -0,0 +1,108 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/TaskMeImg.png";
+import projImg2 from "../assets/img/TaskMeSecImg.png";
+import projImg3 from "../assets/img/TaskMeTirdImg3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "TaskMe",
+      description: "The only aplication for creating tasks you will ever need!",
+      imgUrl: projImg1,
+    } ,
+    {
+      title: "TaskMe",
+      description: "Fast and Secure",
+      imgUrl: projImg2,
+    },
+    {
+      title: "TaskMe",
+      description: "Intuitive Design",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Am inspiered to create more for you!</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">TaskMe</Nav.Link>                     
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second" disabled className="disabled-link">Coming Soon</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third" disabled className="disabled-link">Coming Soon</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first"> {/* firs projec card */}
+                    <p>Am inspiered to create more for you!</p>
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">{/* second projec card */}
+                      <p>Am inspiered to create more for you!!!</p>
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>                     
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third"> {/* third projec card */}
+                      <p>Am inspiered to create more for you!!!</p>
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
